6a3a87582ddd61308769c127db10a9219b38fdb3
Garrick McCaskill <kiwilifter808@gmail.com>
2017-02-20 21:33:25 +1300

    Add recipe select and display details functionality.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe';
 
 
@@ -10,10 +10,17 @@ import { Recipe } from '../recipe';
 export class RecipeListComponent implements OnInit {
 
 	recipes: Recipe[] = [];
+	@Output() recipeSelected = new EventEmitter<Recipe>();
+	
 	recipe = new Recipe('Dummy', 'Dummy', 'https://s-media-cache-ak0.pinimg.com/originals/7d/a4/68/7da46876859879003d27b0118dab8b0b.jpg');
+	
 	constructor() { }
 
 	ngOnInit() {
+	}
+	
+	onSelected(recipe: Recipe){
+		this.recipeSelected.emit(recipe);
 	}
 
 }
